3f3bf266fcc7df98a0223fd37d6478d973922bec
John Wehr <johnwehr@gmail.com>
2021-02-18 18:19:35 -0500

    Add profiling script

---
+++ profile/index.js
@@ -0,0 +1,42 @@
+
+const crypto = require('crypto');
+const {
+  SerializeTransform,
+  DeserializeTransform,
+} = require('../dist');
+
+async function run() {
+  const memoryUsageStart = process.memoryUsage();
+  const maxChunkSize = Math.ceil(1024 + Math.random() * 1024 * 1024);
+  const serializeTransform = new SerializeTransform({ maxChunkSize });
+  const deserializeTransform = new DeserializeTransform();
+  let sentBytes = 0;
+  let receivedBytes = 0;
+  deserializeTransform.on('data', (buffer) => {
+    receivedBytes += buffer.length;
+  });
+  serializeTransform.pipe(deserializeTransform);
+  for (let i = 0; i < 100; i += 1) {
+    const buffer = crypto.randomBytes(Math.ceil(1024 * 1024 * Math.random() * 512));
+    sentBytes += buffer.length;
+    serializeTransform.write(buffer);
+    if(Math.random() < 0.2) {
+      await deserializeTransform.onIdle();
+      const memoryUsageCurrent = process.memoryUsage();
+      console.log(` * Heap usage ${Math.round(100 * (memoryUsageCurrent.heapUsed - memoryUsageStart.heapUsed) / 1024 / 1024) / 100} MB`);
+    }
+  }
+  await deserializeTransform.onIdle();
+  serializeTransform.destroy();
+  deserializeTransform.destroy();
+  if(sentBytes !== receivedBytes) {
+    throw new Error('Sent bytes does not match received bytes');
+  }
+  console.log(`Sent ${Math.round(100 * sentBytes / 1024 / 1024) / 100} MB`);
+  const memoryUsageEnd = process.memoryUsage();
+  for(const key of Object.keys(memoryUsageStart)) {
+    console.log(`${key}: ${Math.round(100 * (memoryUsageEnd[key] - memoryUsageStart[key]) / 1024 / 1024) / 100} MB`);
+  }
+}
+
+run();
